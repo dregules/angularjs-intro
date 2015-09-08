6c5e2a2b359c827685d028044e5f731b7128bc7f
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-09-08 11:19:11 +0100

    added karma

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -1,48 +1,48 @@
-describe('GitUserSearchController', function() {
-  beforeEach(module('GitUserSearch'));
+// describe('GitUserSearchController', function() {
+//   beforeEach(module('GitUserSearch'));
 
-  var ctrl;
+//   var ctrl;
 
-  beforeEach(inject(function($controller) {
-    ctrl = $controller('GitUserSearchController');
-  }));
+//   beforeEach(inject(function($controller) {
+//     ctrl = $controller('GitUserSearchController');
+//   }));
 
-  it('initialises with an empty search result and term', function() {
-    expect(ctrl.searchResult).toBeUndefined();
-    expect(ctrl.searchTerm).toBeUndefined();
-  });
+//   it('initialises with an empty search result and term', function() {
+//     expect(ctrl.searchResult).toBeUndefined();
+//     expect(ctrl.searchTerm).toBeUndefined();
+//   });
 
-  describe('when searching for a user', function() {
+//   describe('when searching for a user', function() {
 
-    var items = [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      }, 
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-    ];
+//     var items = [
+//       {
+//         "login": "tansaku",
+//         "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+//         "html_url": "https://github.com/tansaku"
+//       }, 
+//       {
+//         "login": "stephenlloyd",
+//         "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+//         "html_url": "https://github.com/stephenlloyd"
+//       }
+//     ];
 
-    var httpBackend;
-    beforeEach(inject(function($httpBackend) {
-      httpBackend = $httpBackend;
-      httpBackend
-        .when("GET", "https://api.github.com/search/users?q=hello")
-        .respond(
-          { items: items }
-        );
-    }));
+//     var httpBackend;
+//     beforeEach(inject(function($httpBackend) {
+//       httpBackend = $httpBackend;
+//       httpBackend
+//         .when("GET", "https://api.github.com/search/users?q=hello")
+//         .respond(
+//           { items: items }
+//         );
+//     }));
 
-    it('displays search results', function() {
-      ctrl.searchTerm = 'hello';
-      ctrl.doSearch();
-      httpBackend.flush();
-      expect(ctrl.searchResult.items).toEqual(items);
-    });
-  });
+//     it('displays search results', function() {
+//       ctrl.searchTerm = 'hello';
+//       ctrl.doSearch();
+//       httpBackend.flush();
+//       expect(ctrl.searchResult.items).toEqual(items);
+//     });
+//   });
 
-});
+// });
