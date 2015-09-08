61d4b3c233b4edcd5366a5000210fe385a4bd720
Diego Regules <diegoregules@gmail.com>
2015-09-08 13:55:37 +0100

    Auto Search

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -13,12 +13,22 @@ describe('GitUserSearchController', function() {
   });
 
   describe('when searching for a user', function() {
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=hello")
+        .respond(
+          { items: items }
+        );
+    }));
+
     var items = [
       {
         "login": "tansaku",
         "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
         "html_url": "https://github.com/tansaku"
-      }, 
+      },
       {
         "login": "stephenlloyd",
         "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
@@ -29,6 +39,7 @@ describe('GitUserSearchController', function() {
     it('displays search results', function() {
       ctrl.searchTerm = 'hello';
       ctrl.doSearch();
+      httpBackend.flush();
       expect(ctrl.searchResult.items).toEqual(items);
     });
   });
