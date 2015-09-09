8a6c1c765a9f4aab41544a25927eb093d68f3296
Diego Regules <diegoregules@gmail.com>
2015-09-09 12:10:11 +0100

    Ready for Heroku deployment

---
+++ test/e2e/githubProfileFeature.js
@@ -0,0 +1,27 @@
+describe('Github Profile finder', function() {
+  var searchBox = element(by.model('searchCtrl.searchTerm'));
+  var searchButton = element(by.className('btn'));
+
+  beforeEach(function(){
+    browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github user search');
+  });
+
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike0');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.get(0).getText()).toEqual('spike01');
+
+    //Testing for exact query//
+    // searchBox.sendKeys('spike01');
+    // searchButton.click();
+
+    // expect(element(by.binding('user.login')).getText()).
+    //     toEqual('spike01');
+  // });
+  });
+});
